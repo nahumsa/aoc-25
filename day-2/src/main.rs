043b52c46b6ac4c3a402c1c8172f54e496d20887
nahumsa <nahumsaa@gmail.com>
2025-12-07 23:35:31 -0300

    feat: add day 2 pt 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -15,14 +15,33 @@ fn check_duplicated(val: String) -> bool {
     right == left
 }
 
-fn validate_invalid(min_val: String, max_val: String) -> i64 {
+fn check_duplicated_new_rule(val: String) -> bool {
+    let half_len = val.len() / 2;
+    'outer: for i in 1..=half_len {
+        if val.len() % i == 0 {
+            let left = &val[..i];
+            for j in 1..(val.len() / i) {
+                let right = &val[(i * j)..(i * (j + 1))];
+                if left != right {
+                    continue 'outer;
+                }
+            }
+            return true;
+        }
+    }
+    false
+}
+
+fn validate_invalid(min_val: String, max_val: String, apply_new_rule: bool) -> i64 {
     let mut invalid_number = 0;
 
     let max_number: i64 = max_val.parse().unwrap();
     let min_number: i64 = min_val.parse().unwrap();
     for number in min_number..=max_number {
         let str_number: String = number.to_string();
-        if check_duplicated(str_number) {
+        if check_duplicated(str_number.clone())
+            || (apply_new_rule && check_duplicated_new_rule(str_number))
+        {
             invalid_number += number
         }
     }
@@ -30,24 +49,52 @@ fn validate_invalid(min_val: String, max_val: String) -> i64 {
     invalid_number
 }
 
-fn main() {
-    let file_content = read_file("src/2.txt").unwrap();
-    let binding = file_content.replace("\n", "");
-    let file_content: Vec<&str> = binding.split(",").collect();
+fn part_1(file_content: Vec<&str>) -> i64 {
     let mut total = 0;
-
     for command in file_content {
         let left_right: Vec<&str> = command.split("-").collect();
         dbg!("{:?}", &left_right);
-        let total_invalid =
-            validate_invalid(String::from(left_right[0]), String::from(left_right[1]));
+        let total_invalid = validate_invalid(
+            String::from(left_right[0]),
+            String::from(left_right[1]),
+            false,
+        );
 
         dbg!("{:?}", total_invalid);
 
         total += total_invalid;
     }
+    total
+}
 
-    println!("Second challenge part 1: {:?}", total)
+fn part_2(file_content: Vec<&str>) -> i64 {
+    let mut total = 0;
+    for command in file_content {
+        let left_right: Vec<&str> = command.split("-").collect();
+        dbg!("{:?}", &left_right);
+        let total_invalid = validate_invalid(
+            String::from(left_right[0]),
+            String::from(left_right[1]),
+            true,
+        );
+
+        dbg!("{:?}", total_invalid);
+
+        total += total_invalid;
+    }
+    total
+}
+
+fn main() {
+    let file_content = read_file("src/2.txt").unwrap();
+    let binding = file_content.replace("\n", "");
+    let file_content: Vec<&str> = binding.split(",").collect();
+
+    println!(
+        "Second challenge part 1: {:?}",
+        part_1(file_content.clone())
+    );
+    println!("Second challenge part 2: {:?}", part_2(file_content));
 }
 
 #[cfg(test)]
@@ -59,7 +106,7 @@ mod tests {
         let min = String::from("11");
         let max = String::from("22");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 33);
     }
@@ -69,7 +116,7 @@ mod tests {
         let min = String::from("95");
         let max = String::from("115");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 99);
     }
@@ -79,7 +126,7 @@ mod tests {
         let min = String::from("1188511880");
         let max = String::from("1188511890");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 1188511885);
     }
@@ -89,7 +136,7 @@ mod tests {
         let min = String::from("2121212118");
         let max = String::from("2121212124");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 0);
     }
@@ -99,7 +146,7 @@ mod tests {
         let min = String::from("998");
         let max = String::from("1012");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 1010);
     }
@@ -109,9 +156,19 @@ mod tests {
         let min = String::from("1");
         let max = String::from("19");
 
-        let invalid = validate_invalid(min, max);
+        let invalid = validate_invalid(min, max, false);
 
         assert_eq!(invalid, 11);
+    }
+
+    #[test]
+    fn test_invalid_range_222220_222224() {
+        let min = String::from("222220");
+        let max = String::from("222224");
+
+        let invalid = validate_invalid(min, max, true);
+
+        assert_eq!(invalid, 222222);
     }
 
     #[test]
@@ -130,5 +187,32 @@ mod tests {
         let invalid = check_duplicated(val);
 
         assert!(!invalid);
+    }
+
+    #[test]
+    fn test_check_duplicated_new_rule_true() {
+        let val = String::from("1111111");
+
+        let invalid = check_duplicated_new_rule(val);
+
+        assert!(invalid);
+    }
+
+    #[test]
+    fn test_check_duplicated_new_rule_true_2() {
+        let val = String::from("123123123");
+
+        let invalid = check_duplicated_new_rule(val);
+
+        assert!(invalid);
+    }
+
+    #[test]
+    fn test_check_duplicated_new_rule_true_3() {
+        let val = String::from("121212121212");
+
+        let invalid = check_duplicated_new_rule(val);
+
+        assert!(invalid);
     }
 }
